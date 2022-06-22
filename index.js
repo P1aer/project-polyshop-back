e4eec3c4824e92e04d527dcef8e7c41eeefb5f9c
Player173 <arhoangel@bk.ru>
2022-06-22 19:49:45 +0300

    add userSchema

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from "express";
 import fs from "fs";
 import https from "https"
 import http from "http"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+mongoose.connect(
+    "mongodb+srv://admin:ember12345@cluster0.suo4q.mongodb.net/?retryWrites=true&w=majority"
+).then(() => console.log("start DB MONGA"))
 
 const key = fs.readFileSync("./.cert/localhost-key.pem", "utf-8");
 const cert = fs.readFileSync("./.cert/localhost.pem", "utf-8");
@@ -18,7 +24,17 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+app.post("/auth/login",(req,res) => {
+    const body = req.body;
+    const token = jwt.sign({
+        email: body.email,
+        password: body.password,
+        },"project")
+    res.json({
+        success: true,
+        token
+    })
+})
 /*const httpServer = http.createServer(app)*/
 const httpsServer = https.createServer(credentials, app);
 /*httpServer.listen(8080)*/
