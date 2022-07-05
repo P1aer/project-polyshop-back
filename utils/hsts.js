69b0580c3a02011638cd99ff479e5f8d3d99a2d8
Ilya Osipov <arhoangel@bk.ru>
2022-07-05 18:53:02 +0300

    minior fixes

--- utils/hsts.js
+++ utils/hsts.js
@@ -1,5 +1,5 @@
 function hsts(req, res, next) {
-    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    res.setHeader('Strict-Transport-Security', 'max-age=300000; includeSubDomains; preload');
     next()
 }
 
